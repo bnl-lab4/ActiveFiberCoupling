0cf48607d238d500c5d0b92b916d2393c8a014cd
Aaron Mueninghoff <mueninghoffa@gmail.com>
2025-10-08 22:02:40 -0400

    Post-merge cleanup, deleting and untracking unused files

--- center_axes.py
+++
@@ -1,7 +0,0 @@
-from motion import move
-import time, photodiode_in
-
-def run(ser, ch, file):
-    move('x', 37.5, ser, ch, file)
-    move('y', 37.5, ser, ch, file)
-    move('z', 37.5, ser, ch, file)
