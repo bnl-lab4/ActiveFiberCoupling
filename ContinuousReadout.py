5577eea2f84779c14d7f8ab8cbd984078aa6eb24
Aaron Mueninghoff <mueninghoffa@gmail.com>
2025-12-24 11:52:38 -0800

    Docstring added for ContinuousReadout.py

--- ContinuousReadout.py
+++ ContinuousReadout.py
@@ -1,3 +1,7 @@
+"""
+Continuously print the sensor reading until interrupted.
+"""
+
 import logging
 from typing import Union
 from MovementClasses import StageDevices
@@ -7,8 +11,22 @@ log = logging.getLogger(__name__)
 
 
 def run(stage: StageDevices, exposureTime: Union[int, float], avg: bool = True):
-    log.info(f"Initiating continuous readout of {stage.name} sensor with" +
-             f" exposure time {exposureTime} and avg = {avg}")
+    """
+    Continuously print the sensor output with the supplied exposure time.
+
+    Parameters
+    ----------
+    stage : `MovementClasses.StageDevices`
+        The stage whose sensor will be read.
+    exposureTime : int, float
+        Exposure time for which to integrate the sensor.
+    avg : bool, default=True
+        Whether to average the sensor readout over the integration time.
+    """
+    log.info(
+        f"Initiating continuous readout of {stage.name} sensor with"
+        + f" exposure time {exposureTime} and avg = {avg}"
+    )
     try:
         while True:
             intensity = stage.integrate(exposureTime, avg)
