024274eb82895cddb88856260f8e506675867a87
Aaron Mueninghoff <mueninghoffa@gmail.com>
2026-01-18 21:41:16 -0800

    Added docstrings and a few type hints (not comprehensive) to hill_climb.py

--- hill_climb.py
+++ hill_climb.py
@@ -1,3 +1,7 @@
+"""
+Optimize fiber position by repeatedly hill climbing with finer and finer step size.
+"""
+
 ############### TODO
 # recursive hill climbing
 # climb in an arbitrary direction (vector input)
@@ -14,6 +18,7 @@ from typing import List, Optional, Sequence, Tuple, Type, Union
 import matplotlib.pyplot as plt
 import numpy as np
 import sigfig
+from matplotlib.figure import Figure
 
 from logging_utils import get_logger
 from movement_classes import Distance, MovementType, StageDevices
@@ -33,7 +38,51 @@ def plot_climber(
     init_position: Distance,
     init_positive: bool,
     movement_type: MovementType,
-):
+) -> Figure:
+    """
+    Plot the results of a series of hill climbs from `hill_climber` for a
+    single axix.
+
+    For each separate hill climb, plots the position vs sensor reading for
+    each point and generates a legend with the step size and the position
+    of the first and last positions. Recalculates the fiber positions based
+    on the shape of `climber_results` and other args.
+
+    Parameters
+    ----------
+    climber_results : list of np.ndarrays
+        A list containing the results array from each call to `hill_climb`.
+    stagename : str
+        Name of the stage; used in the plot suptitle.
+    axis : {'x', 'y', 'z'}
+        Name of the axis which was climbed; used in the plot suptitle.
+    init_step : distance.Distance
+        Initial step size of the first hill climb.
+    step_factor : float
+        Step factor used to adjust step size after each climb.
+    init_position : distance.Distance
+        Initial fiber position. Used to calculate the fiber position at
+        each step.
+    init_positive : bool
+        Whether the first hill climb was in the positive (``True``) or
+        negative (``False``) direction.
+    movement_type : movement_classes.MovementType
+        Which movement type was used; used in the plot suptitle.
+
+    Returns
+    -------
+    matplotlib.figure.Figure
+
+    See Also
+    --------
+    hill_climber : calls this if the plot is to be shown or logged
+
+    Notes
+    -----
+    This calculates the fiber positions because `hill_climb` uses ``move``
+    commands, not ``goto`` commands. The logic in this function must be
+    kept up to date (as the inverse) with `hill_climb` and `hill_climber`.
+    """
     position = init_position
     step = init_step
     if not init_positive:
@@ -139,6 +188,58 @@ def hill_climb(
     Ndecrease: int,
     exposure_time: Union[int, float],
 ):
+    """
+    Hill climb along a single step size with a single step size.
+
+    Repeatedly moves `axis` by `step` and reads sensor until the sensor
+    reading is below the highest recording for `Ndecrease` steps. Both
+    `Ndecrease` and `softening` are intended to allow for increased
+    robustness against low SNR measurements.
+    Parameters
+    ----------
+    stage : movement_classes.StageDevices
+        The stage to hill climb with.
+    axis : {'x', 'y', 'z'}
+        Single character str denoting the axis to hill climb on.
+    movement_type : movement_classes.MovementType
+        Which movement type to move with. Only ``PIEZO`` and ``STEPPER``
+        are supported; ``GENERAL`` may be added at a later date.
+    step : distance.Distance
+        Step size.
+    max_steps : int
+        Maximum number of steps to take before stopping.
+    softening : float
+        Multiplicative factor on previous sensor values for determining
+        whether to halt. Intended to help combat sensor noise.
+    Ndecrease : int
+        How many readings below the previous maximum to require before
+        halting. Intended to help combat sensor noise.
+    exposure_time : int or float
+        What exposure time to use for the sensor readings.
+
+    Returns
+    -------
+    np.ndarray
+        Array of sensor readings from each step.
+    bool
+        Whether the function halted before reaching `max_steps`.
+
+    See Also
+    --------
+    hill_climber : repeatedly calls `hill_climb` with decreasing step size.
+
+    Notes
+    -----
+    Currently, `Ndecrease` only determines how many consecutive steps have
+    to be taken where without finding a new maximum. The readings below the
+    maximum do not need to monotonically decrease, though. It would be
+    preferable to have a more robust system to account for noise and
+    require monotonically decreasing measurements to halt. As it is now,
+    `softening` is too simple and not very effective.
+
+    See the "Notes" section of `run` regarding future additions of
+    recursive hiil climbing.
+    """
     logger.info(
         f"Hill climbing on {stage.name} axis {axis} {movement_type.value}"
         + f" with step size {step.prettyprint()} and exposure time {exposure_time}"
@@ -183,7 +284,70 @@ def hill_climber(
     min_step: Distance,
     show_plot: bool = False,
     log_plot: bool = True,
-):
+) -> None:
+    """
+    Hill climb along an axis with successively smaller step sizes to find a
+    local maximum.
+
+    Repeatedly call `hill_climb`, each time decreasing the step size by
+    `step_factor` and switching the direction, until one of the relative or
+    absolute tolerances are met or the minimum step size or maximum number
+    of climbs are reached.
+
+    Parameters
+    ----------
+    stage : movement_classes.StageDevices
+        The stage to hill climb on.
+    axis : {'x', 'y', 'z'}
+        Single character str denoting the axis to hill climb on.
+    exposure_time : float or int
+        The exposure time to use for reading the sensor at each step.
+    movement_type : movement_classes.MovementType
+        Which movement type to move with. Only ``PIEZO`` and ``STEPPER``
+        are supported; ``GENERAL`` may be added at a later date.
+    init_step : distance.Distance
+        The initial step size to use for the first climb.
+    step_factor : float
+        The amount by which to decrease the step size for each subsequent climb.
+    init_positive : bool
+        Whether to initially climb in the positive (``True``) or negative
+        (``False``) direction.
+    rel_tol : float
+        Maximum ratio between previous and current hill climb peak values
+        for which to halt.
+    softening : float
+        Multiplicative factor on previous sensor values for determining
+        whether to halt. Intended to help combat sensor noise.
+    abs_tol : distance.Distance
+        Maximum difference between previous and current hill climb peak
+        values for which to halt.
+    Ndecrease : int
+        How many readings below the previous maximum to require before
+        halting. Intended to help combat sensor noise.
+    max_climbs : int
+        Maximum number of times to call `hill_climb`.
+    max_steps : int
+        Maximum number of steps to take in a single climb. Passed to `hill_climb`.
+    min_step : distance.Distance
+        Minimum step size to use without halting.
+    show_plot : bool, default=False
+        Whether to show a plot summarizing all hill climbs once halted. The
+        plot blocks the script from continuing until it is closed.
+    log_plot : bool, default=True
+        Whether to save a plot summarizing all hill climbs. Saved in "./log_plots/".
+
+    See Also
+    --------
+    hill_climb : hill climbs once with a constant step size
+
+    Notes
+    -----
+    See the "Notes" section for `hill_climb` for a discussion on
+    `softening` and `Ndecrease`.
+
+    See the "Notes" section of `run` regarding future additions of
+    recursive hiil climbing.
+    """
     assert (
         movement_type != MovementType.GENERAL
     ), "General movement hill climb may be added later"
@@ -302,6 +466,43 @@ def arg_check(
     extra: Optional[Callable] = None,
     extra_text: str = "",
 ):
+    """
+    Used to duck-type many of the arguments of `run`.
+
+    Duck-types `arg` into a list of `argtype`s if `arg` is not already a
+    list. Restrictions on the value(s) of `arg` can be supplied via
+    `extra`.
+
+    Parameters
+    ----------
+    arg : any
+        The argument to check and duck type.
+    argname : str
+        The name of the arg, for a potential error message.
+    argtype : Type
+        The type that `arg` or its elements should be.
+    axes : str or list thereof
+        `arg` is duck-typed to be the same length as `axes`.
+    extra : callable, optional
+        A function to place restrictions on the value(s) of `arg`. Must
+        accept `arg` and return a bool.
+    extra_text : str, default=""
+        Additional text for a potential error message corresponding to the
+        logic of `extra`.
+
+    Returns
+    -------
+    list of type `argtype`
+        This will be the same length as `axes`.
+
+    Raises
+    ------
+    ValueError
+        Raised if:
+            * `arg` is not `argtype` or a list thereof
+            * `arg` is a list that is not the same length as `axes`
+            * `arg` or its elements do not satisfy `extra`
+    """
     if extra is None:
         extra = lambda x: True  # noqa E731
 
@@ -341,7 +542,77 @@ def run(
     order: Optional[Tuple[int, ...]] = None,
     show_plot: Union[bool, Sequence[bool]] = False,
     log_plot: Union[bool, Sequence[bool]] = True,
-):
+) -> None:
+    """
+    Hill climb along one or more axes (individually) with successively smaller step sizes.
+
+    Calls `hill_climber` on one or more axes. Almost all args can be given
+    a single value to apply to all axes or a list of values, one for each axis.
+
+    Parameters
+    ----------
+    stage : movement_classes.StageDevices
+        The stage to hill climb on.
+    movement_type : movement_classes.MovementType or list thereof
+        Which movement type(s) to move with. Only ``PIEZO`` and ``STEPPER``
+        are supported; ``GENERAL`` may be added at a later date.
+    exposure_time : float, int, or list thereof
+        The exposure time(s) to use for reading the sensor at each step.
+    axes : str or list thereof, default=None
+        String or list of axis names (``x``, ``y``, or ``z``) to hill climb
+        on, in order. ``None`` defaults to ``"xzy"``.
+    init_step : distance.Distance or list thereof, optional
+        The initial step size(s) to use for the first climb. ``None``
+        defaults to 1 or 100 microns for movement types ``PIEZO`` or
+        ``STEPPER``, respectively.
+    step_factor : float or list thereof, default=0.5
+        The amount(s) by which to decrease the step size for each subsequent climb.
+    init_positive : bool or list thereof, default=True
+        Whether to initially climb in the positive (``True``) or negative
+        (``False``) direction(s).
+    rel_tol : float or list thereof, default=0.01
+        Maximum ratio(s) between previous and current hill climb peak values
+        for which to halt.
+    softening : float or list thereof, default=0
+        Multiplicative factor(s) on previous sensor values for determining
+        whether to halt. Intended to help combat sensor noise.
+    abs_tol : distance.Distance or list thereof, default=0
+        Maximum difference(s) between previous and current hill climb peak
+        values for which to halt.
+    Ndecrease : int or list thereof, default=1
+        How many readings below the previous maximum to require before
+        halting. Intended to help combat sensor noise.
+    max_climbs : int or list thereof, default=16
+        Maximum number of times to call `hill_climb`.
+    max_steps : int or list thereof, default=100
+        Maximum number of steps to take in a single climb. Passed to `hill_climb`.
+    min_step : distance.Distance or list thereof, default=None
+        Minimum step size(s) to use without halting. ``None`` defaults to 0.1 volts or 1 microstep for movemente types ``PIEZO`` or ``STEPPER``, respectively.
+    order : ``None``
+        Reserved for future use with recursion.
+    show_plot : bool or list thereof, default=False
+        Whether to show a plot(s) summarizing all hill climbs once halted. The
+        plot(s) blocks the script from continuing until they are closed.
+    log_plot : bool or list thereof, default=True
+        Whether to save a plot(s) summarizing all hill climbs. Saved in "./log_plots/".
+
+    See Also
+    --------
+    arg_check : duck types each argument as needed
+    hill_climber : called on each individual axis
+
+    Notes
+    -----
+    See the "Notes" section for `hill_climb` for a discussion on
+    `softening` and `Ndecrease`.
+
+    In the future, this module will be updated to allow for recursive hill
+    climbing, in which `run` is called on one or more axes between each
+    step-movement and sensor reading in `hill_climb` for another axis. This
+    could be used to hill climb in the focus direction while hill climbing
+    in the transverse directions after each step to remain centered on the
+    beam, for example.
+    """
     # axes are the axes you want to hill climb on
     # movement_type can be general, and must either be a single or have same length as axes
     # init_step is the initial step size either for all axes or for each axis
